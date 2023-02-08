e3d6c7cb4801ba54123833d311629ad36799e15f
huboh <72440222+huboh@users.noreply.github.com>
2023-02-09 00:17:26 +0100

    add lib function to normalize tribute and fetch slider images

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,27 @@
 import { join } from "path";
-import { SiteData } from "@/types";
-import { readFile } from "fs/promises";
+import { SiteData, Tribute } from "@/types";
+import { readFile, readdir } from "fs/promises";
+import { Record, default as PocketBase } from "pocketbase";
 
 export const getSiteData = async (directory?: string): Promise<SiteData> => {
-  const path = join(directory || "/", "index.json");
-  const data = await readFile(path, { encoding: "utf-8" });
+  return JSON.parse(await readFile(join(directory || "/", "index.json"), { encoding: "utf-8" })) ?? [];
+};
 
-  return JSON.parse(data) ?? [];
+export const getSliderImagesPaths = async () => {
+  return (await readdir(join(process.cwd(), "public/assets/images/slider-images"))).map((path) => `assets/images/slider-images/${path}`);
+};
+
+export const normalizeTribute = (record: Record, client: PocketBase): Tribute => {
+  return {
+    id: record.id,
+    name: record.name,
+    email: record.email,
+    title: record.title,
+    message: record.message,
+    updated: record.updated,
+    created: record.created,
+    relationShip: record.relationShip,
+    displayImage: record.displayImage && client.getFileUrl(record, record.displayImage),
+    tributeImage: record.tributeImage && client.getFileUrl(record, record.tributeImage),
+  };
 };
