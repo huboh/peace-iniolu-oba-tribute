90d476911ba9b95baf7caabe0d6de1ad12b2adf7
huboh <72440222+huboh@users.noreply.github.com>
2023-02-09 00:14:25 +0100

    update config files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects: async () => ([
+    {
+      source: "/",
+      permanent: false,
+      destination: "/tributes",
+    }
+  ]),
+  images: {
+    remotePatterns: [
+      {
+        port: '8090',
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        pathname: '/api/files/**',
+      }
+    ]
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
